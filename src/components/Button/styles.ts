adcb4f197a67879f1297d306e3823e1aae1aca27
douglas godois dos santos <doug_potter92@hotmail.com>
2023-01-21 17:45:44 -0300

    first commit

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+
+export const ButtonContainer = styled.button`
+    width: 100%;
+    height: 42px;
+    background-color: #87CEFA;;
+    color: #FFF;
+
+    border: 1px solid #87CEFA;;
+    border-radius: 21px;
+
+    &:hover {
+        opacity: 0.6;
+        cursor:pointer;
+    }
+`
---
+++ src/global.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: 'Nunito', sans-serif;
+        box-sizing:  border-box;
+    }
+
+    body {
+        font-family: 'Nunito', sans-serif;
+        margin: 0;
+        padding: 0;
+        background-color: #008080;
+    }
+
+     button, textarea, h1, h2, h3, h4, h5, h6 {
+        padding:0 35%;
+        font-family: 'Nunito', sans-serif;
+    }
+    input{
+        padding:0 10%;
+    }
+`
